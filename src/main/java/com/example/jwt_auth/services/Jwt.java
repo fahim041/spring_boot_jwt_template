4a3ffa9c0c8812c88c16e09f29a6fd742486702e
Fahim090 <fahimalsafi@gmail.com>
2025-08-29 17:30:15 -0600

    jwt services

---
+++ src/main/java/com/example/jwt_auth/services/Jwt.java
@@ -0,0 +1,34 @@
+package com.example.jwt_auth.services;
+
+import com.example.jwt_auth.entites.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class Jwt {
+    private final Claims claims;
+    private final SecretKey secretKey;
+
+    public Jwt(Claims claims, SecretKey secretKey) {
+        this.claims = claims;
+        this.secretKey = secretKey;
+    }
+
+    public boolean isExpired() {
+        return claims.getExpiration().after(new Date());
+    }
+
+    public Long getUserId() {
+        return Long.valueOf(claims.getSubject());
+    }
+
+    public Role getRole() {
+        return Role.valueOf(claims.get("role", String.class));
+    }
+
+    public String toString() {
+        return Jwts.builder().claims(claims).signWith(secretKey).compact();
+    }
+}
