70e97018c5736bea232d262bcd37e2a4b878cd31
Fahim090 <fahimalsafi@gmail.com>
2025-08-29 10:41:38 -0600

    user entity

---
+++ src/main/java/com/example/jwt_auth/entites/Role.java
@@ -0,0 +1,6 @@
+package com.example.jwt_auth.entites;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/jwt_auth/entites/User.java
@@ -0,0 +1,22 @@
+package com.example.jwt_auth.entites;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+}
